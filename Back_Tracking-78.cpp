a802d26d91aefa0a3167d81e798f70bab8c4e361
Yiqun Wang <yiqunw0907@gmail.com>
2019-08-22 09:34:31 -0400

    bit operation

--- Back_Tracking-78.cpp
+++ Back_Tracking-78.cpp
@@ -38,3 +38,25 @@ private:
         generate(i+1, nums, item, result);
     }
 };
+
+class Solution2{
+public:
+    vector<vector<int> > subsets(vector<int> &nums){
+        vector<vector<int> > result;
+
+        int all_set = 1 << nums.size();
+
+        for (int i = 0; i < all_set; i++){
+            vector<int> item;
+            for(int j = 0; j < nums.size(); j++){
+                if ( i & (1 << j)){
+                    item.push_back(nums[j]);
+                }
+            }
+
+            result.push_back(item);
+        }
+
+        return result;
+    }
+};
