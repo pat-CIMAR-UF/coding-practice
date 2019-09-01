8110ebb83e1c64a78cbfe395a486d72904b2a6b4
Yiqun Wang <yiqunw0907@gmail.com>
2019-08-31 22:55:50 -0400

    Update Data-215.cpp

--- Data-215.cpp
+++ Data-215.cpp
@@ -14,11 +14,21 @@
 #include <math.h>
 #include <stack>
 
+
 using namespace std;
 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        
+        priority_queue<int, vector<int>, greater<int> > Q;
+        for (int i = 0; i <nums.size(); i++){
+            if (Q.size() < k) Q.push(nums[i]);
+            else if ( nums[i] > Q.top()){
+                Q.pop();
+                Q.push(nums[i]);
+            }
+        }
+
+        return Q.top();
     }
 };
