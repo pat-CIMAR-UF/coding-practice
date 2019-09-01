e00a885bec5a54a588e4676b80f9b99edb4017c9
Yiqun Wang <yiqunw0907@gmail.com>
2019-09-01 11:51:44 -0400

    Update Hash&String-49.cpp

--- Hash&String-49.cpp
+++ Hash&String-49.cpp
@@ -16,3 +16,29 @@
 
 using namespace std;
 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string> > anagram;
+        vector<vector<string> > result;
+
+        for (int i = 0; i < strs.size(); i++){
+            string str = strs[i];
+            sort(str.begin(), str.end());
+
+            if(anagram.find(str) == anagram.end()){
+                vector<string> item;
+                anagram[str] = item;
+            }
+
+            anagram[str].push_back(strs[i]);
+        }
+
+        map<string, vector<string> >::iterator it;
+        for(it = anagram.begin(); it != anagram.end(); it++){
+            result.push_back((*it).second);
+        }
+
+        return result;
+    }
+};
