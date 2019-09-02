1a8b9c12adbe68664c549245338121722694da0c
Yiqun Wang <yiqunw0907@gmail.com>
2019-09-02 18:13:08 -0400

    1

--- DP-300.cpp
+++ DP-300.cpp
@@ -43,3 +43,31 @@ public:
         return LIS;
     }
 };
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.size() == 0){
+            return 0;
+        }
+
+        vector<int> stack;
+        stack.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++){
+            if(nums[i] > stack.back()){
+                stack.push_back(nums[i]);
+            }
+
+            else{
+                for (int j = 0; j < stack.size(); j++){
+                    if (stack[j] >= nums[i]){
+                        stack[j] = nums[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+        return stack.size();
+    }
+};
