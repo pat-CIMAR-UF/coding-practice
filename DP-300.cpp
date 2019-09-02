8e5056b5e6492121f81ab5d08e9a2bf081235a52
Yiqun Wang <yiqunw0907@gmail.com>
2019-09-02 17:52:22 -0400

    Update DP-300.cpp

--- DP-300.cpp
+++ DP-300.cpp
@@ -19,6 +19,27 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
+        if (nums.size() == 0){
+            return 0;
+        }
+
+        vector<int> dp(nums.size(), 0);
+        dp[0] = 1;
+        int LIS = 1;
+        for (int i = 1; i < dp.size(); i++){
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++){
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+
+            if (LIS < dp[i]){
+                LIS = dp[i];
+            }
+        }
+
+        return LIS;
     }
 };
